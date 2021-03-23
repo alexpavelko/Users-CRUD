7c9015a7c9d0e79a01e89d0cbdd40298a2be1c81
alexpavelko <pavelkobond@gmail.com>
2021-03-23 21:00:33 +0200

    get all buttons and required fields

---
+++ CRUD.js
@@ -0,0 +1,9 @@
+var firstName = document.querySelector('#userFName');   
+var lastName = document.querySelector('#userLName');
+var userPassword = document.querySelector('#userPass');
+var birthDate = document.querySelector('#userBDay');
+var form = document.querySelector('#form-Add-User');
+var table =  document.querySelector('#mainTable');
+var btnUpdate = document.querySelector("#btn-update");
+form.style.display = 'none'; 
+
