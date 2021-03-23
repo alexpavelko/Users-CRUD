57953765c3fe12aaf67d1bd13ae4b9c3c03fc7ca
alexpavelko <pavelkobond@gmail.com>
2021-03-23 21:03:34 +0200

    get extra buttons and their 'click' events

--- CRUD.js
+++ CRUD.js
@@ -11,3 +11,30 @@ userPassword.addEventListener('input',() => {
     userPassword.value = userPassword.value.replace(/[а-я]/gi,'');
 });
 
+
+
+var btnRegNewUser = document.querySelector('#btn-submit');
+btnRegNewUser.addEventListener('click',()=>{   
+    formValidate();
+    let type = "POST";
+    let url = "http://localhost:56481/api/Users";
+    
+    AddUserRequest(type,url);
+    window.location.reload(); 
+});
+
+var btnAddUser = document.querySelector("#btn-sign-in");
+btnAddUser.addEventListener('click',() =>{
+    form.style.display = 'block';
+    btnRegNewUser.style.display = 'block';
+    btnUpdate.style.display = 'none';
+});
+
+var appExit = document.querySelector("#btn-exit");
+appExit.addEventListener('click',() =>{
+    table.style.display = 'none';
+    form.style.display = 'none';
+    btnRegNewUser.style.display = 'none';
+    btnUpdate.style.display = 'none';
+});
+
